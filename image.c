a54aa1231916d425e6a8286608cc0613e168d50c
Zacharie McCormick <zacfire1998@hotmail.fr>
2020-11-27 23:41:06 -0500

    Converted Waveshare Arduino code to PIC16 C code, it works really well. I might add other features if I ever find the datasheet for this screen

---
+++ image.c
@@ -0,0 +1,13 @@
+/*
+ * File:   image.c
+ * Author: zacfi
+ *
+ * Created on November 27, 2020, 10:57 PM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,13 +1,104 @@
+/**
+  Generated main.c file from MPLAB Code Configurator
+
+  @Company
+    Microchip Technology Inc.
+
+  @File Name
+    main.c
+
+  @Summary
+    This is the generated main.c using PIC24 / dsPIC33 / PIC32MM MCUs.
+
+  @Description
+    This source file provides main entry point for system initialization and application code development.
+    Generation Information :
+        Product Revision  :  PIC24 / dsPIC33 / PIC32MM MCUs - 1.169.0
+        Device            :  PIC24FJ64GA002
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 v1.50
+        MPLAB 	          :  MPLAB X v5.40
+*/
+
 /*
- * File:   main.c
- * Author: zacfi
- *
- * Created on November 27, 2020, 8:33 PM
+    (c) 2020 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+/**
+  Section: Included Files
+*/
+#include <stdio.h>
+
+#include "mcc_generated_files/system.h"
+#include "mcc_generated_files/pin_manager.h"
+#include "mcc_generated_files/delay.h"
+#include "waveshare29epaper.h"
+#include "mcc_generated_files/drivers/spi_master.h"
+#include "images.h"
+
+/*
+                         Main application
  */
+int main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+    if(!spi_master_open(MASTER1))
+		// Not able to open SPI, call fail() or optionally do something 
+		// else waiting for the SPI to become free
+		return 0; 
+    DELAY_milliseconds(1000);
+    LED_SetHigh();
+    printf("Initializing the display...\n");
+    InitDisplay(lut_full_update);
+    printf("Clearing the frame memory...\n");
+    ClearFrameMemory(0xFF);
+    printf("Clearing the display...\n");
+    DisplayFrame();
+    DELAY_milliseconds(1000);
+    InitDisplay(lut_partial_update);
+    SetFrameMemoryFull(run1, false);
+    DisplayFrame();
+    SetFrameMemoryFull(run2, false);
+    DisplayFrame();
+    SetFrameMemoryFull(run3, false);
+    DisplayFrame();
+    SetFrameMemoryFull(run4, false);
+    DisplayFrame();
+    SetFrameMemoryFull(run5, false);
+    DisplayFrame();
+    SetFrameMemoryFull(run6, false);
+    DisplayFrame();
+    
+    while (1)
+    {
+        LED_SetHigh();
+        DELAY_milliseconds(500);
+        LED_SetLow();
+        DELAY_milliseconds(500);
+    }
 
-
-#include "xc.h"
-
-int main(void) {
-    return 0;
+    return 1;
 }
+/**
+ End of File
+*/
+
